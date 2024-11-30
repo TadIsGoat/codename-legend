64cee7fbde5b38a2076427e59f8ac9c1471a1c8e
TadIsGoat <tadeasiknet@gmail.com>
2024-11-30 22:58:20 +0100

    Base player movement

--- Legend of The Blade/Assets/scripts/PlayerController.cs
+++ Legend of The Blade/Assets/scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System.Threading;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -5,21 +6,48 @@ public class PlayerController : MonoBehaviour
     #region VARIABLES
 
     [HideInInspector] public Rigidbody2D rb;
-    [HideInInspector] public Vector2 movementInput;
+    [HideInInspector] public Collider2D col;
+
+    [Header("Timers")]
+    public float interval = 0.0001f; //better not to play with
+    private float timer = 0;
+
+    [Header("Movement")]
+    [HideInInspector] public Vector2 movementInput; //input from PlayerInput class
+    public float maxRunSpeed = 10f;
+    private static float lerpValue = 0.5f;
+    [Range(0, 10)] public float runAccel = 10f; //values outside of Range may be problematic
+    [Range(0, 20)] public float runDeccel = 20f; //values outside of Range may be problematic
 
     #endregion
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        col = GetComponent<Collider2D>();
     }
 
     void Update()
     {
-        rb.AddForce(movementInput, ForceMode2D.Force);
-    }
+        timer -= Time.deltaTime;
 
-    public void Movement()
-    {
-        
+        if (timer < 0)
+        {
+            #region AWSD movement
+            timer = interval;
+
+            Vector2 targetSpeed = movementInput * maxRunSpeed;
+            targetSpeed = new Vector2(Mathf.Lerp(rb.linearVelocity.x, targetSpeed.x, lerpValue), Mathf.Lerp(rb.linearVelocity.y, targetSpeed.y, lerpValue));
+
+            float accelRateX = Mathf.Abs(targetSpeed.x) > 0.01f ? runAccel: runDeccel;
+            float accelRateY = Mathf.Abs(targetSpeed.y) > 0.01f ? runAccel : runDeccel;
+
+            Vector2 accelRate = new Vector2(accelRateX, accelRateY);
+
+            Vector2 speedDiff = targetSpeed - rb.linearVelocity;
+            Vector2 movement = speedDiff * accelRate;
+            rb.AddForce(movement, ForceMode2D.Force);
+            #endregion
+        }
     }
 }
