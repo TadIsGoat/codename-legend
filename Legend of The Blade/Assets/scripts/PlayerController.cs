37336368e56891062275231b7d3ce1d9b4d77040
TadIsGoat <tadeasiknet@gmail.com>
2024-12-04 21:09:09 +0100

    Player controller upgrade + basic health script

--- Legend of The Blade/Assets/scripts/PlayerController.cs
+++ Legend of The Blade/Assets/scripts/PlayerController.cs
@@ -1,4 +1,3 @@
-using System.Threading;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -8,16 +7,12 @@ public class PlayerController : MonoBehaviour
     [HideInInspector] public Rigidbody2D rb;
     [HideInInspector] public Collider2D col;
 
-    [Header("Timers")]
-    [SerializeField]private float interval = 0.0001f; //better not played with
-    private float timer = 0;
-
     [Header("Movement")]
     [HideInInspector] public Vector2 movementInput; //input from PlayerInput class
     [SerializeField] public float maxRunSpeed = 10f;
     private static float lerpValue = 0.5f;
-    [Range(1, 10)][SerializeField] private float runAccel = 5f; //values outside of Range may be problematic
-    [Range(1, 10)][SerializeField] private float runDeccel = 3f; //values outside of Range may be problematic
+    [Range(1, 100)][SerializeField] private float runAccel = 35f; //values outside of Range may be problematic
+    [Range(1, 100)][SerializeField] private float runDeccel = 20f; //values outside of Range may be problematic
 
     #endregion
 
@@ -27,24 +22,18 @@ public class PlayerController : MonoBehaviour
         col = GetComponent<Collider2D>();
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        timer -= Time.deltaTime;
+        #region WASD movement
+        Vector2 targetSpeed = movementInput * maxRunSpeed;
+        targetSpeed = new Vector2(Mathf.Lerp(rb.linearVelocity.x, targetSpeed.x, lerpValue), Mathf.Lerp(rb.linearVelocity.y, targetSpeed.y, lerpValue));
 
-        if (timer < 0)
-        {
-            #region WASD movement
-            timer = interval;
+        Vector2 accelRate = new Vector2(Mathf.Abs(targetSpeed.x) > 1 ? runAccel : runDeccel, Mathf.Abs(targetSpeed.y) > 1 ? runAccel : runDeccel);
 
-            Vector2 targetSpeed = movementInput * maxRunSpeed;
-            targetSpeed = new Vector2(Mathf.Lerp(rb.linearVelocity.x, targetSpeed.x, lerpValue), Mathf.Lerp(rb.linearVelocity.y, targetSpeed.y, lerpValue));
+        Vector2 speedDiff = targetSpeed - rb.linearVelocity;
+        Vector2 movement = speedDiff * accelRate;
+        rb.AddForce(movement, ForceMode2D.Force);
+        #endregion
 
-            Vector2 accelRate = new Vector2(Mathf.Abs(targetSpeed.x) > 1 ? runAccel : runDeccel, Mathf.Abs(targetSpeed.y) > 1 ? runAccel : runDeccel);
-
-            Vector2 speedDiff = targetSpeed - rb.linearVelocity;
-            Vector2 movement = speedDiff * accelRate;
-            rb.AddForce(movement, ForceMode2D.Force);
-            #endregion
-        }
     }
 }
---
+++ Legend of The Blade/Assets/scripts/healthScript.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class healthScript : MonoBehaviour
+{
+    Rigidbody2D rb;
+    private float currentHealth = 0;
+    [SerializeField] private float maxHealth = 100;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
+        Debug.Log(currentHealth);
+    }
+
+    public void TakeHit(float damage, float knockback, Vector2 sourcePos)
+    {
+        currentHealth = Mathf.Clamp(currentHealth - damage, 0, maxHealth);
+        Debug.Log(currentHealth);
+
+        rb.AddForce(-sourcePos * knockback, ForceMode2D.Impulse);
+
+        if (currentHealth <= 0)
+        {
+            Debug.Log("Dead");
+        }
+    }
+}
