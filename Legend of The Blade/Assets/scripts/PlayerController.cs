f04156b7c594687629450dafdff93d5745edb1e4
TadIsGoat <tadeasiknet@gmail.com>
2024-12-04 19:24:11 +0100

    accel/deccel fix

--- Legend of The Blade/Assets/scripts/PlayerController.cs
+++ Legend of The Blade/Assets/scripts/PlayerController.cs
@@ -9,15 +9,15 @@ public class PlayerController : MonoBehaviour
     [HideInInspector] public Collider2D col;
 
     [Header("Timers")]
-    public float interval = 0.0001f; //better not to play with
+    [SerializeField]private float interval = 0.0001f; //better not played with
     private float timer = 0;
 
     [Header("Movement")]
     [HideInInspector] public Vector2 movementInput; //input from PlayerInput class
-    public float maxRunSpeed = 10f;
+    [SerializeField] public float maxRunSpeed = 10f;
     private static float lerpValue = 0.5f;
-    [Range(0, 10)] public float runAccel = 10f; //values outside of Range may be problematic
-    [Range(0, 20)] public float runDeccel = 20f; //values outside of Range may be problematic
+    [Range(1, 10)][SerializeField] private float runAccel = 5f; //values outside of Range may be problematic
+    [Range(1, 10)][SerializeField] private float runDeccel = 3f; //values outside of Range may be problematic
 
     #endregion
 
@@ -39,10 +39,7 @@ public class PlayerController : MonoBehaviour
             Vector2 targetSpeed = movementInput * maxRunSpeed;
             targetSpeed = new Vector2(Mathf.Lerp(rb.linearVelocity.x, targetSpeed.x, lerpValue), Mathf.Lerp(rb.linearVelocity.y, targetSpeed.y, lerpValue));
 
-            float accelRateX = Mathf.Abs(targetSpeed.x) > 0.01f ? runAccel: runDeccel;
-            float accelRateY = Mathf.Abs(targetSpeed.y) > 0.01f ? runAccel : runDeccel;
-
-            Vector2 accelRate = new Vector2(accelRateX, accelRateY);
+            Vector2 accelRate = new Vector2(Mathf.Abs(targetSpeed.x) > 1 ? runAccel : runDeccel, Mathf.Abs(targetSpeed.y) > 1 ? runAccel : runDeccel);
 
             Vector2 speedDiff = targetSpeed - rb.linearVelocity;
             Vector2 movement = speedDiff * accelRate;
