795834fa7662e0d5454198c95d5253c64e29137b
TadIsGoat <tadeasiknet@gmail.com>
2025-03-27 08:50:07 +0100

    .

--- The Blade/Assets/scripts/scriptable objects/CharacterData.cs
+++ The Blade/Assets/scripts/scriptable objects/CharacterData.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public abstract class CharacterData : ScriptableObject
@@ -9,9 +10,42 @@ public abstract class CharacterData : ScriptableObject
     [SerializeField] [Range(1, 100)][Tooltip("values outside of Range may be problematic")] public float runDeccel;
     [SerializeField] [Range(0, 1)][Tooltip("If the target speed is gonna fall closer to current velocity or (max run speed * input)")] public float lerpValue;
 
-    public virtual Directions Direction { get; set; }
-
-    public enum Directions {
-
+    public enum Directions
+    {
+        N,
+        S,
+        E,
+        W,
+        SW,
+        SE,
+        NW,
+        NE
     }
+
+    public enum Anims //west anims not needed because we can just flip the EAST anim
+    {
+        idle_S,
+        idle_N,
+        idle_E,
+        idle_SE,
+        idle_NE,
+
+        walk_S,
+        walk_N,
+        walk_E,
+        walk_SE,
+        walk_NE,
+
+        attack_S,
+        attack_N,
+        attack_E,
+        attack_SE,
+        attack_NE,
+    }
+
+    public virtual Dictionary<Directions, Anims> idleAnims { get; set; }
+
+    public virtual Dictionary<Directions, Anims> walkAnims { get; set; }
+
+    public virtual Dictionary<Directions, Anims> attackAnims { get; set; }
 }
--- The Blade/Assets/scripts/scriptable objects/PlayerData.cs
+++ The Blade/Assets/scripts/scriptable objects/PlayerData.cs
@@ -2,84 +2,62 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "PlayerData", menuName = "ScriptableObjects/PlayerData", order = 1)]
-public class PlayerData :  CharacterData //object-specific variables are set here
+public class PlayerData : CharacterData //object-specific variables are set here
 {
-    /*
-    [Header("Movement")]
-    [SerializeField] public override float maxRunSpeed = 10f;
-    [SerializeField] public float deccelTreshhold = 0.1f;
-    [SerializeField][Range(1, 100)][Tooltip("values outside of Range may be problematic")] public float runAccel = 35f;
-    [SerializeField][Range(1, 100)][Tooltip("values outside of Range may be problematic")] public float runDeccel = 1f;
-    [SerializeField][Range(0, 1)][Tooltip("If the target speed is gonna fall closer to current velocity or (max run speed * input)")] public float lerpValue  = 0.5f;
-    */
-    public override Directions Direction { get; set; } = Directions.N;
-
-    public enum Directions
+    public override Dictionary<Directions, Anims> idleAnims
     {
-        N,
-        S,
-        E,
-        W,
-        SW,
-        SE,
-        NW,
-        NE
+        get => base.idleAnims;
+        set => base.idleAnims = value;
     }
 
-    public enum Anims //west anims not needed because we can just flip the EAST anim
+        public override Dictionary<Directions, Anims> walkAnims
     {
-        idle_S,
-        idle_N,
-        idle_E,
-        idle_SE,
-        idle_NE,
-
-        walk_S,
-        walk_N,
-        walk_E,
-        walk_SE,
-        walk_NE,
-
-        attack_S,
-        attack_N,
-        attack_E,
-        attack_SE,
-        attack_NE,
+        get => base.walkAnims;
+        set => base.walkAnims = value;
     }
 
-    public Dictionary<Directions, Anims> idleAnims = new Dictionary<Directions, Anims>
+        public override Dictionary<Directions, Anims> attackAnims
     {
-        { Directions.S, Anims.idle_S },
-        { Directions.N, Anims.idle_N },
-        { Directions.E, Anims.idle_E },
-        { Directions.W, Anims.idle_E }, //because we can just flip the EAST anim
-        { Directions.SE, Anims.idle_SE },
-        { Directions.SW, Anims.idle_SE }, //because we can just flip the EAST anim
-        { Directions.NE, Anims.idle_NE },
-        { Directions.NW, Anims.idle_E }, //because we can just flip the EAST anim
-    };
+        get => base.attackAnims;
+        set => base.attackAnims = value;
+    }
 
-    public Dictionary<Directions, Anims> walkAnims = new Dictionary<Directions, Anims>
+    public PlayerData()
     {
-        { Directions.S, Anims.walk_S },
-        { Directions.N, Anims.walk_N },
-        { Directions.E, Anims.walk_E },
-        { Directions.W, Anims.walk_E }, //because we can just flip the EAST anim
-        { Directions.SE, Anims.walk_SE },
-        { Directions.SW, Anims.walk_SE }, //because we can just flip the EAST anim
-        { Directions.NE, Anims.walk_NE },
-        { Directions.NW, Anims.walk_NE }, //because we can just flip the EAST anim
-    };
+        idleAnims = new Dictionary<Directions, Anims>
+        {
+            { Directions.S, Anims.idle_S },
+            { Directions.N, Anims.idle_N },
+            { Directions.E, Anims.idle_E },
+            { Directions.W, Anims.idle_E },
+            { Directions.SE, Anims.idle_SE },
+            { Directions.SW, Anims.idle_SE },
+            { Directions.NE, Anims.idle_NE },
+            { Directions.NW, Anims.idle_E },
+        };
 
-    public Dictionary<Directions, Anims> attackAnims = new Dictionary<Directions, Anims>
-    {
-        { Directions.S, Anims.attack_S },
-        { Directions.N, Anims.attack_N },
-        { Directions.E, Anims.attack_E },
-        { Directions.W, Anims.attack_E }, //because we can just flip the EAST anim
-        { Directions.SE, Anims.attack_SE },
-        { Directions.SW, Anims.attack_SE }, //because we can just flip the EAST anim
-        { Directions.NE, Anims.attack_NE },
-        { Directions.NW, Anims.attack_NE }, //because we can just flip the EAST anim
-    };
+        walkAnims = new Dictionary<Directions, Anims>
+        {
+            { Directions.S, Anims.walk_S },
+            { Directions.N, Anims.walk_N },
+            { Directions.E, Anims.walk_E },
+            { Directions.W, Anims.walk_E },
+            { Directions.SE, Anims.walk_SE },
+            { Directions.SW, Anims.walk_SE },
+            { Directions.NE, Anims.walk_NE },
+            { Directions.NW, Anims.walk_NE },
+        };
+
+        attackAnims = new Dictionary<Directions, Anims>
+        {
+            { Directions.S, Anims.attack_S },
+            { Directions.N, Anims.attack_N },
+            { Directions.E, Anims.attack_E },
+            { Directions.W, Anims.attack_E },
+            { Directions.SE, Anims.attack_SE },
+            { Directions.SW, Anims.attack_SE },
+            { Directions.NE, Anims.attack_NE },
+            { Directions.NW, Anims.attack_NE },
+        };
+    }
 }
